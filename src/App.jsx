33ec478faf66b62197559a2c82b49bf7e821db20
Ryan Deist <ryandeist@gmail.com>
2025-01-29 15:45:19 -0500

    wrapped <App/> in Context for User

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useContext, useState, useEffect } from 'react'
-import { Routes, Route, useNavigate } from 'react-router'
-
-import NavBar from './components/NavBar/NavBar'
+import { useContext } from 'react'
+import { Routes, Route } from 'react-router'
+import './App.css'
+import NavBar from './components/NavBar/NavBar';
+// import SignUpForm from './components/SignUpForm/SignUpForm';
+// import SignInForm from './components/SignInForm/SignInForm';
+// import Landing from './components/Landing/Landing';
+// import Dashboard from './components/Dashboard/Dashboard';
+import { UserContext } from './contexts/UserContext';
 
 const App = () => {
+
+  const { user } = useContext(UserContext);
 
   return (
     <>
       <NavBar />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
