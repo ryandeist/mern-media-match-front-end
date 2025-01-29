0af47af3801af4e1b617f1df395778f9b5b217b7
JoseMdna <jose73001@gmail.com>
2025-01-28 16:59:11 -0800

    sign in and sign out components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
-const App = () => {
-  return <h1>Empty Pizza Box Media Match!</h1>;
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import SignUp from './components/SignUp'
+import SignIn from './components/SignIn'
 
-};
+const App = () => {
+  return (
+    <Router>
+      <nav>
+        <Link to="/sign-up">Sign Up</Link>
+        <Link to="/sign-in">Sign In</Link>
+      </nav>
+      <Routes>
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/" element={<h1>Empty Pizza Box Media Match!</h1>} />
+      </Routes>
+    </Router>
+  )
+}
 
 export default App
