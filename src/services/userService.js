c13faa46267e9d41395fa82a38602591dea69759
Adam Paley <apaley95@gmail.com>
2025-01-29 17:41:23 -0500

    correct vite env variable

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,4 @@
-const BASE_URL = `${import.meta.env.VITE_BASE_URL}/users`
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/users`
 const API_KEY = import.meta.env.VITE_API_KEY
 const API_URL = import.meta.env.API_URL
 const CLIENT_ID = import.meta.env.CLIENT_ID
