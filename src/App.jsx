c3a73209f72d6788e917a3083fe8e19c267f1b40
Adam Paley <apaley95@gmail.com>
2025-01-29 23:54:25 -0500

    created basic game card component

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,41 @@ import Landing  from './components/Landing/Landing';
 import './App.css'
 import { UserContext } from './contexts/UserContext'
 import SettingsComponent from './components/SettingsComponent/SettingsComponent'
+import CardComponent from './components/CardComponent/CardComponent';
 
 const App = () => {
   // hooks
   const { user } = useContext(UserContext)
   // state variable
   const [settings, setSettings] = useState([])
-  const [gameData, setGameData] = useState({})
+  const [gameData, setGameData] = useState([])
 
+  useEffect(()=> {
+    // fetch function
+    const fetchData = async () => {
+      const fetchedData = await showGame( settings )
+      console.log('Fetched Data', fetchedData)
+      setGameData(fetchedData)
+    }
+    fetchData()
+  }, [settings])
+  
   // fetch function
   const fetchData = async () => {
-    const data = await showGame( settings )
-    console.log('Data', data)
-    setGameData(data)
+    const fetchedData = await showGame( settings )
+    console.log('Fetched Data', fetchedData)
+    setGameData(fetchedData)
   }
 
   return (
 
     <>
       <NavBar />
-      {!user ?  (
+      {user ?  (
         <Landing />
       ) :  (
         <>
+          <CardComponent gameData={gameData} />
           <button onClick={fetchData}>Fetch Data</button>
           <SettingsComponent settings={settings} setSettings={setSettings} />
         </>  
--- src/components/CardComponent/CardComponent.jsx
+++ src/components/CardComponent/CardComponent.jsx
@@ -4,11 +4,16 @@ import { useState } from "react"
 // component
 const CardComponent = (props) => {
 
-    
+    const showProp = () => {
+        console.log(props)
+    }
     // return 
     return (
         <div>
-            <img alt="cover art of product" />
+            <img src={props.gameData[0]?.cover} alt="cover image for product"/>
+                {props.gameData[0]?.name}
+                {props.gameData[0]?.parentalRating}
+                ${props.gameData[0]?.price}
         </div>
     )
 }
