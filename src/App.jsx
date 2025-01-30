c9cf78e7a57cb9499ba41efa4424b925d35cdad1
JoseMdna <jose73001@gmail.com>
2025-01-29 18:32:48 -0800

    updated signup/signin handle change function

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,29 @@ import { useContext, useState, useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router';
 import { showGame } from './services/userService';
 import NavBar from './components/NavBar/NavBar';
-import Landing  from './components/Landing/Landing';
-import './App.css'
+import Landing from './components/Landing/Landing';
+import SignUpForm from './components/SignUpForm/SignUpForm';
+import SignInForm from './components/SignInForm/SignInForm';
+import './App.css';
 import { UserContext } from './contexts/UserContext';
 
 const App = () => {
-  // state variable
-  const [settings, setSettings] = useState(["Indie"])
+  const [settings, setSettings] = useState(["Indie"]);
+  const { user } = useContext(UserContext);
 
   const fetchData = async () => {
-    const data = await showGame( settings )
-    console.log('Data:', data)
-  }
-
-  const { user } = useContext(UserContext);
+    const data = await showGame(settings);
+    console.log('Data:', data);
+  };
 
   return (
     <>
       <NavBar />
-      <Landing />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
+        <Route path="/sign-in" element={<SignInForm />} />
+      </Routes>
       <button onClick={fetchData}>Fetch Data</button>
     </>
   );
